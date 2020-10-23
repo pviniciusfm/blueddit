cef5892b77918c7903f5e42612bb41f971306892
Paulo Machado <pmachado@fanatics.com>
2020-10-22 19:44:05 -0700

    Scaffolding of graphql findAll posts

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,38 @@
-import { MikroORM } from "@mikro-orm/core"
-import { __prod__ } from "./constants"
-import { Post } from "./entities/Post"
-import microConfig from "./mikro-orm.config" 
+import "reflect-metadata"
+import { MikroORM } from "@mikro-orm/core";
+import { ApolloServer } from "apollo-server-express";
+import express from "express";
+import { buildSchema } from "type-graphql";
+import microConfig from "./mikro-orm.config";
+import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/post";
 
-const main = async () => {;
-	const orm = await MikroORM.init(microConfig)
-	await orm.getMigrator().up()
-	
-	const post = orm.em.create(Post, {title: 'my first post'})
-	await orm.em.persistAndFlush(post)
+const main = async () => {
+  const orm = await MikroORM.init(microConfig);
+  await orm.getMigrator().up();
 
-	console.log('-------sql 2--------------------')
-	await orm.em.nativeInsert(Post, {title: "post 2"})
-}
+  const app = express();
+  app.listen(4000, () => {
+    console.log("Blueddit is live on localhost:4000");
+  });
 
+  app.get("/", (_, res) => {
+    res.send("Hello");
+  });
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver],
+      validate: false,
+    }),
+	context: () => ({em: orm.em})
+  });
+
+  apolloServer.applyMiddleware({ app });
+  // await orm.em.persistAndFlush(post)
+  // const posts = await orm.em.find(Post, {});
+};
 
 main().catch((err) => {
-	console.error(err)
-})
+  console.error(err);
+});
